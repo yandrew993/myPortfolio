f6331ac4a199c0ce57b525734ba904b067e7e88b
Andrew Young <yandrew993@gmail.com>
2024-09-24 04:40:59 -0400

    changes added

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["images.pexels.com"], // Add the allowed external domain
+  },
+};
 
 export default nextConfig;
