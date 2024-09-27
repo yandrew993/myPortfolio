698c18717b1e1369af274d29d9507f2a131a1add
Andrew Young <yandrew993@gmail.com>
2024-09-27 08:42:57 -0400

    Maintenance code added

--- src/app/update/page.jsx
+++ src/app/update/page.jsx
@@ -6,9 +6,7 @@ const Update = () => {
   return (
     <div className="flex flex-col min-h-screen bg-gray-100">
       <div className="flex-grow flex flex-col items-center justify-center">
-        <h1 className="text-4xl font-bold mb-4 sm:text-2xl">
-          We'll be back soon!
-        </h1>
+        <h1 className="text-4xl font-bold mb-4 sm:text-2xl">Han'g tight!😬</h1>
         <p className="text-lg text-gray-700 sm:text-font-light pr-4 pl-4">
           Sorry for the inconvenience but we're performing some maintenance at
           the moment. We'll be back online shortly!
